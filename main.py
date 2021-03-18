8028ec687c21c417510e9df1e712c473d1aedbff
Youngchan <michael_cho77@naver.com>
2021-03-18 23:43:04 +0900

    Add - Getting Total Cases by Country

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import pandas as pd
 
 daily_df = pd.read_csv("data/daily_report.csv")
 
+# 전체 확진자, 사망자, 회복자 수
 totals_df = daily_df[["Confirmed", "Deaths", "Recovered"]].sum().reset_index(name="count")
 totals_df = totals_df.rename(columns={'index': "coundition"})
 
 
+# 도시를 그룹화 한후 해당 도시의 확진자, 사망자, 회복자 수
+countries__df = daily_df[["Country_Region", "Confirmed", "Deaths", "Recovered"]]
+countries__df = daily_df.groupby("Country_Region").sum().reset_index()
